65968501e529a4be89f66cfbf2b4637c5cb49a89
gigi1993 <tuttobene.luigi@gmail.com>
2017-08-14 22:18:08 +0200

    improved usability

--- website/urls.py
+++ website/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^participate$', views.participate, name='participate'),
-    url(r'^overview$', views.overview, name='overview'),
-    url(r'^myfirstout$', views.myfirstout, name='myfirstout'),
-    url(r'^line$', views.line, name='line'),
+#    url(r'^$', views.home, name='home'),
+#    url(r'^participate$', views.participate, name='participate'),
+    url(r'^$', views.overview, name='overview'),
+    url(r'^about$', views.about, name='about'),
+    url(r'^previewNL$', views.previewNL, name='previewNL'),
+    url(r'^previewUK$', views.previewUK, name='previewUK'),
+    url(r'^UK$', views.UK, name='UK'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'website/home.html', {})
-def participate(request):
-    return render(request, 'website/participate.html', {})
+#def home(request):
+#    return render(request, 'website/home.html', {})
+#def participate(request):
+#    return render(request, 'website/participate.html', {})
 def overview(request):
     return render(request, 'website/overview.html', {})
-def myfirstout(request):
-    return render(request, 'website/myfirstout.html', {})
-def line(request):
-    return render(request, 'website/line.html', {})
+def about(request):
+    return render(request, 'website/about.html', {})
+def previewNL(request):
+    return render(request, 'website/previewNL.html', {})
+def previewUK(request):
+    return render(request, 'website/previewUK.html', {})
+def UK(request):
+    return render(request, 'website/UK.html', {})
