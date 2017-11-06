8a9b464927367d1ddeada671e570625c6ea79ec7
gigi1993 <tuttobene.luigi@gmail.com>
2017-11-06 21:43:26 +0100

    Restructured pages, and more.

--- website/urls.py
+++ website/urls.py
@@ -2,11 +2,16 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-#    url(r'^$', views.home, name='home'),
-#    url(r'^participate$', views.participate, name='participate'),
+
     url(r'^$', views.overview, name='overview'),
     url(r'^about$', views.about, name='about'),
-    url(r'^previewNL$', views.previewNL, name='previewNL'),
-    url(r'^previewUK$', views.previewUK, name='previewUK'),
-    url(r'^UK$', views.UK, name='UK'),
+    url(r'^details_AS_UK$', views.details_AS_UK, name='details_AS_UK'),
+    url(r'^details_AS_DE$', views.details_AS_DE, name='details_AS_DE'),
+    url(r'^details_TAB_UK$', views.details_TAB_UK, name='details_TAB_UK'),
+    url(r'^details_TAB_DE$', views.details_TAB_DE, name='details_TAB_NL'),
+#    url(r'^$', views.home, name='home'),
+#    url(r'^participate$', views.participate, name='participate'),
+#    url(r'^previewNL$', views.previewNL, name='previewNL'),
+#    url(r'^previewUK$', views.previewUK, name='previewUK'),
+#    url(r'^UK$', views.UK, name='UK'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
-
-#def home(request):
-#    return render(request, 'website/home.html', {})
-#def participate(request):
-#    return render(request, 'website/participate.html', {})
 def overview(request):
     return render(request, 'website/overview.html', {})
 def about(request):
     return render(request, 'website/about.html', {})
-def previewNL(request):
-    return render(request, 'website/previewNL.html', {})
-def previewUK(request):
-    return render(request, 'website/previewUK.html', {})
-def UK(request):
-    return render(request, 'website/UK.html', {})
+def details_AS_UK(request):
+    return render(request, 'website/details_AS_UK.html', {})
+def details_AS_DE(request):
+    return render(request, 'website/details_AS_DE.html', {})
+def details_TAB_UK(request):
+    return render(request, 'website/details_TAB_UK.html', {})
+def details_TAB_DE(request):
+    return render(request, 'website/details_TAB_DE.html', {})
+#def home(request):
+#    return render(request, 'website/home.html', {})
+#def participate(request):
+#    return render(request, 'website/participate.html', {})
+#def previewNL(request):
+#    return render(request, 'website/previewNL.html', {})
+#def previewUK(request):
+#    return render(request, 'website/previewUK.html', {})
+#def UK(request):
+#    return render(request, 'website/UK.html', {})
